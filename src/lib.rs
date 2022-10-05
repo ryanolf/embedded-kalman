b07708111c012bfff8a2a7ca8ee685a7e8efb176
Jostein Løwer <jostlowe@gmail.com>
2022-10-05 14:13:06 +0200

    commit before first publishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-//! Wolololol
-
 /*!
 # embedded-kalman
 
@@ -8,21 +6,29 @@
 * Embedded or resource-constrained systems
 * High-reliability-systems where runtime failure is not an option
 
+Although this library was designed with embedded systems in mind, there's nothing wrong with using
+it in other applications. On the contrary, it should be quite fast on unconstrained systems.
 For now, **embedded-kalman** only features a standard optimal gain Kalman filter for
 linear time-invariant systems. Future versions are planned to contain:
 
 * Different variations such as the time-variant version of the regular Kalman Filter
 * Kalman filter varieties for non-linear systems such as the Extended Kalman Filter (EKF) and the Unscented Kalman Filter (UKF)
 
-## Using **embedded-kalman**
-You will need the last stable build of the [rust compiler](https://www.rust-lang.org)
-and the official package manager: [cargo](https://github.com/rust-lang/cargo).
-
-Simply add the following to your `Cargo.toml` file:
-
+## A quick example
 ```ignore
-[dependencies]
-embedded-kalman = "0.1.0"
+use embedded_kalman::KalmanFilter;
+use nalgebra::{SMatrix, SVector, Vector2};
+
+let kf: KalmanFilter<f64, 2, 2, 2, _> = KalmanFilter::new(
+    ... // matrices and initial state go here
+);
+
+let kf = kf
+    .predict(Vector2::new(1.0, 0.0)) // Give an input to the system
+    .update(Vector2::new(1.0, 0.0)) // Update the state from a given measurement
+    .expect("Update step failed!");
+
+let (x_posterior, P_posterior) = kf.get_posteriors(); // Get the posteriors
 ```
 
 ## Features
@@ -38,13 +44,23 @@ embedded-kalman = "0.1.0"
 * Thus far, little effort has been made to optimize the code in this library
 * This library is pretty new and still not battle-tested. Please do not use this for anything important
 
+## Using **embedded-kalman**
+You will need the last stable build of the [rust compiler](https://www.rust-lang.org)
+and the official package manager: [cargo](https://github.com/rust-lang/cargo).
+
+Simply add the following to your `Cargo.toml` file:
+
+```ignore
+[dependencies]
+embedded-kalman = "0.1.0"
+```
+
 ## Implemented Filters
 * [Kalman Filter](KalmanFilter)
 
 */
 
 
-/*
 #![deny(
     missing_docs,
     missing_debug_implementations,
@@ -56,18 +72,17 @@ embedded-kalman = "0.1.0"
     unused_import_braces,
     unused_qualifications
 )]
-*/
 
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![no_std]
 
 use nalgebra::{RealField, SMatrix, SVector};
-
 
 #[derive(Debug)]
 struct Constants<T, const Nx: usize, const Nz: usize, const Nu: usize> {
     // State transition matrices
-    A: SMatrix<T, Nx, Nx>,
+    F: SMatrix<T, Nx, Nx>,
     B: SMatrix<T, Nx, Nu>,
 
     // Measurement matrix
@@ -92,7 +107,6 @@ struct Predict<T, const Nx: usize, const Nz: usize>
     x_prior: SVector<T, Nx>,
 }
 
-#[derive(Debug)]
 struct Update<T, const Nx: usize, const Nz: usize>
     where
         T: RealField,
@@ -107,6 +121,7 @@ struct Update<T, const Nx: usize, const Nz: usize>
     K: SMatrix<T, Nx, Nz>,
 }
 
+/// A marker trait to indicate what state the Kalman filter is currently in
 pub trait State {}
 impl<T, const Nx: usize, const Nz: usize> State for Predict<T, Nx, Nz> where T: RealField {}
 impl<T, const Nx: usize, const Nz: usize> State for Update<T, Nx, Nz> where T: RealField {}
@@ -135,10 +150,12 @@ impl<T, const Nx: usize, const Nz: usize> State for Update<T, Nx, Nz> where T: R
 ///
 /// ## Usage
 /// Let's make a (terribly uninteresting) instance of the Kalman Filter. We use mostly identity matrices here, for the sake of exemplification.
-/// Replace these matrices with something more interesting
+/// Replace these matrices with something more interesting. Note that the notation and names used for the Kalman Filter
+/// is similar to the one in the [Wikipedia artice about the Kalman Filter](https://en.wikipedia.org/wiki/Kalman_filter) if you're in doubt which matrix is which
 ///
 /// ```
 /// use embedded_kalman::KalmanFilter;
+/// use nalgebra::{SMatrix, SVector};
 /// let kf: KalmanFilter<f64, 2, 2, 2, _> = KalmanFilter::new(
 ///     SMatrix::identity(), // A
 ///     SMatrix::identity(), // B
@@ -149,7 +166,40 @@ impl<T, const Nx: usize, const Nz: usize> State for Update<T, Nx, Nz> where T: R
 ///     SMatrix::identity(), // P0
 /// );
 /// ```
-
+/// **embedded-kalman** can infer the dimensions of the matrices in the parameters in most cases.
+/// If the dimensions of the matrices do not match, the Kalman Filter won't compile
+///
+/// Two main methods are used to iterate the KalmanFilter: [KalmanFilter::update] and [KalmanFilter::predict].
+///
+/// ```
+/// use embedded_kalman::KalmanFilter;
+/// use nalgebra::Vector2;
+/// let kf = kf
+///     .predict(Vector2::new(1.0, 0.0)) // Give an input to the system
+///     .update(Vector2::new(1.0, 0.0)) // Update the state from a given measurement
+///     .expect("Innovation matrix cannot be inverted");
+/// ```
+/// If the Kalman Filter is in the `Update` state, only the `predict` method is available.
+/// If the filter is in the `Predict` state, only the `update` method is available, preserving
+/// the 2-step recurrent nature of the Kalman Filter.
+///
+/// The update method is failable and might return [Option::None] if the innovation matrix is
+/// non-invertible. This happens due to rounding errors when the process noise covariance `Q` is small,
+/// and is rare.
+/// The prior state and state covariance can be fetched after the `predict` stage
+/// The posterior state and state covariance can be fetched after the `update` stage
+///
+/// ```
+/// use embedded_kalman::KalmanFilter;
+/// use nalgebra::Vector2;
+///
+/// let kf = kf.predict(Vector2::new(1.0, 0.0)); // Give an input to the system
+/// let (x_prior, P_prior) = kf.get_priors(); // Get the priors
+/// let kf = kf
+///     .update(Vector2::new(1.0, 0.0)) // Update the state from a given measurement
+///     .expect("Innovation matrix cannot be inverted");
+/// let (x_posterior, P_posterior) = kf.get_posteriors(); // Get the posteriors
+/// ```
 
 #[derive(Debug)]
 pub struct KalmanFilter<T, const Nx: usize, const Nz: usize, const Nu: usize, S>
@@ -166,19 +216,25 @@ KalmanFilter<T, Nx, Nz, Nu, Update<T, Nx, Nx>>
     where
         T: RealField,
 {
+
+    /// Gets the posterior state `x` and posterior state covariance `P` as a tuple.
+    /// This method is only available when the Kalman Filter is in the `Update` state
     pub fn get_posteriors(&self) -> (&SVector<T, Nx>, &SMatrix<T, Nx, Nx>) {
         (&self.state.x_posterior, &self.state.P_posterior)
     }
 
+    /// Use this method to perform the predict step of a Kalman Filter iteration,
+    /// given an input vector `u`
+    /// This method is only available when the Kalman Filter is in the `Update` state
     pub fn predict(self, u: SVector<T, Nu>) -> KalmanFilter<T, Nx, Nz, Nu, Predict<T, Nx, Nz>> {
-        let Constants { A, B, Q, .. } = &self.constants;
+        let Constants { F, B, Q, .. } = &self.constants;
         let Update {
             x_posterior,
             P_posterior,
             ..
         } = &self.state;
-        let x_prior = A * x_posterior + B * u;
-        let P_prior = A * P_posterior * A.transpose() + Q;
+        let x_prior = F * x_posterior + B * u;
+        let P_prior = F * P_posterior * F.transpose() + Q;
 
         KalmanFilter {
             constants: self.constants,
@@ -186,8 +242,9 @@ KalmanFilter<T, Nx, Nz, Nu, Update<T, Nx, Nx>>
         }
     }
 
+    /// Creates a new Kalman Filter. Look at the top-level documentation for usage and examples
     pub fn new(
-        A: SMatrix<T, Nx, Nx>,
+        F: SMatrix<T, Nx, Nx>,
         B: SMatrix<T, Nx, Nu>,
         H: SMatrix<T, Nz, Nx>,
         Q: SMatrix<T, Nx, Nx>,
@@ -196,7 +253,7 @@ KalmanFilter<T, Nx, Nz, Nu, Update<T, Nx, Nx>>
         P0: SMatrix<T, Nx, Nx>,
     ) -> Self {
         Self {
-            constants: Constants { A, B, H, Q, R },
+            constants: Constants { F, B, H, Q, R },
             state: Update {
                 x_posterior: x0,
                 P_posterior: P0,
@@ -211,10 +268,16 @@ KalmanFilter<T, Nx, Nz, Nu, Predict<T, Nx, Nx>>
     where
         T: RealField,
 {
+    /// Gets the prior state `x_prior` and prior state covariance `P_prior` as a tuple.
+    /// This method is only available when the Kalman Filter is in the `Predict` state
     pub fn get_priors(&self) -> (&SVector<T, Nx>, &SMatrix<T, Nx, Nx>) {
         (&self.state.x_prior, &self.state.P_prior)
     }
 
+    /// Use this method to perform the update step of a Kalman Filter iteration,
+    /// given a measurement vector `z`. This method returns [None] when the innovation matrix
+    /// can't be inverted.
+    /// This method is only available when the Kalman Filter is in the `Update` state
     pub fn update(self, z: SVector<T, Nz>) -> Option<KalmanFilter<T, Nx, Nz, Nu, Update<T, Nx, Nz>>> {
         let Constants { H, R, .. } = &self.constants;
         let Predict {
@@ -248,7 +311,7 @@ KalmanFilter<T, Nx, Nz, Nu, Predict<T, Nx, Nx>>
 #[cfg(test)]
 mod tests {
     use super::*;
-    use nalgebra::{Matrix2, Vector2};
+    use nalgebra::Vector2;
 
     #[test]
     fn it_works() {
@@ -262,7 +325,7 @@ mod tests {
             SMatrix::identity(),
         );
 
-        let (x, P) = kf
+        let (_x, _P) = kf
             .predict(Vector2::new(1.0, 0.0))
             .update(Vector2::new(1.0, 0.0))
             .expect("Innovation matrix cannot be inverted")
